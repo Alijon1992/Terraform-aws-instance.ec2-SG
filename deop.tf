d528b0f91088b6ed95f1735917ed0f9af2059263
Alijon1992 <121365233+Alijon1992@users.noreply.github.com>
2023-03-12 00:31:37 -0500

    Add files via upload

---
+++ deop.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = terraform.workspace == "deop" ? "ca-central-1" : "ca-central-1"
+}
+
+//Get Linux AMI ID using SSM Parameter endpoint in ca-central-1
+data "aws_ssm_parameter" "linuxAmi" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+//Create and bootstrap EC2 in ca-central-1
+resource "aws_instance" "ec2-vm" {
+  ami                         = data.aws_ssm_parameter.linuxAmi.value
+  instance_type               = "t3.micro"
+  associate_public_ip_address = true
+  tags = {
+    Name = "${terraform.workspace}-ec2"
+  }
+}
