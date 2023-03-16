36b3ee2a7c990468cff11eb3039ccf07163e3318
Ali <Egebiomm@gmail.com>
2023-03-15 23:59:56 -0400

    changed instance location from "ca-central-1" to "us-east-1" and misspelled correction-"K" to "C"

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-  region = terraform.workspace == "deop" ? "ca-central-1" : "ca-central-1"
+  region = terraform.workspace == "deop" ? "us-east-1" : "us-east-1"
 }
 
-//Get Linux AMI ID using SSM Parameter endpoint in ca-central-1
+//Get Linux AMI ID using SSM Parameter endpoint in us-east-1
 data "aws_ssm_parameter" "linuxAmi" {
   name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
 
-//Kreate and bootstrap EC2 in ca-central-1
+//Create and bootstrap EC2 in us-east-1
 resource "aws_instance" "ec2-vm" {
   ami                         = data.aws_ssm_parameter.linuxAmi.value
   instance_type               = "t3.micro"
